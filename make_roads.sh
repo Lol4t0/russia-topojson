9d8a794037c9b8f43a0e508cbbe154ec5173fdea
Ilya Sidorov <sidorov.ij@gmail.com>
2014-09-19 15:14:53 +0400

    longest roads

--- make_roads.sh
+++ make_roads.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
 jq -c '.features[].geometry.coordinates | reduce .[] as $item ([]; . + $item)' $@ | tr -d "[]"
+cat roads.csv | awk '{ print length, -bash }' | sort -r -n -T /data/tmp/ | cut -d" " -f2- | head -n 100000 > roads_longest.csv
